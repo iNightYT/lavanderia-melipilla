3dd1fb51bcaa6147d9bcb1743f954760145d6b62
iNightYT <gmartinezbahamondes@gmail.com>
2023-11-07 19:17:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lavanderia Melipilla',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
